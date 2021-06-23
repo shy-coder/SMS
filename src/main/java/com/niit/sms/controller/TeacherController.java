dc6429ab73b042d87e48668860c07f884f292c45
jianjun-hub <1792679781@qq.com>
2021-06-23 17:04:15 +0800

    教师模块完成

--- src/main/java/com/niit/sms/controller/TeacherController.java
+++ src/main/java/com/niit/sms/controller/TeacherController.java
@@ -1,11 +1,10 @@
 package com.niit.sms.controller;
-
 import com.niit.sms.bean.Teacher;
 import com.niit.sms.service.TeacherService;
+import com.niit.sms.utils.MD5Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,7 +20,7 @@ import java.util.Map;
 public class TeacherController {
     @Autowired
     private TeacherService teacherService;
-    
+
     @RequestMapping("/teacherListData")
     @ResponseBody
     public Object teacherListData(){
@@ -36,16 +35,47 @@ public class TeacherController {
     @ResponseBody
     public Object deleteTeacher(@RequestParam("id") Integer teaId){
         Map<String, Object> result = new HashMap<>();
-            Teacher teacher = new Teacher();
-            teacher.setId(teaId);
-            Integer status = teacherService.deleteTeacher(teacher);
-            result.put("status",status);
-            return result;
+        Teacher teacher = new Teacher();
+        teacher.setId(teaId);
+        Integer status = teacherService.deleteTeacher(teacher);
+        result.put("status",status);
+        return result;
     }
 
     @RequestMapping("/teaInfo")
     public String teaInfo(){
         return "/teacher/teaInfo";
+    }
+
+    @RequestMapping("/upload")
+    @ResponseBody
+    public Object updateTeacher( Teacher teacher){
+        Integer status = teacherService.updateTeacher(teacher);
+        Map<String, Object> result = new HashMap<>();
+        result.put("status",status);
+        return result;
+    }
+
+    @RequestMapping("/insert")
+    @ResponseBody
+    public Object insertTeacher( Teacher teacher){
+        Map<String, Object> result = new HashMap<>();
+        Teacher teacher1 = teacherService.findByTno(teacher);
+        if(teacher1 != null){
+            result.put("status",0);
+            result.put("msg","工号重复了");
+            return  result;
+        }else {
+            if(teacher.getPassword().isEmpty()){
+                teacher.setPassword(MD5Util.MD5Lower("123456"));
+            }else {
+                teacher.setPassword(MD5Util.MD5Lower(teacher.getPassword()));
+            }
+            Integer status = teacherService.insertTeacher(teacher);
+            result.put("status",status);
+            result.put("msg","");
+            return result;
+        }
     }
 
 }
